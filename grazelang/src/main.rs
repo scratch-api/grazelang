54188f9ea2d854949ca805dceeef2294d54a9ac2
TheCommCraft <79996518+TheCommCraft@users.noreply.github.com>
2026-06-24 16:54:12 +0200

    remove old main.rs logic

--- grazelang/src/main.rs
+++ grazelang/src/main.rs
@@ -4,31 +4,4 @@ use grazelang::cli::input::Cli;
 fn main() {
     let cli = Cli::parse();
     cli.execute();
-    // let lexer = lexer::create_lexer(include_str!(r#"C:\Users\gine\projects\grazelang\grazelang\src\test3.graze"#));
-    // let mut context = ParseContext::new(Default::default(), Default::default());
-
-    // let parsed = parser::parse_graze_program(&mut PeekableLexer::new(lexer), &mut context).unwrap();
-
-    // if !context.successful {
-    //     for message in &context.messages {
-    //         dbg!(message);
-    //     }
-    //     dbg!(parsed);
-    //     panic!("Parsing unsuccessful.");
-    // }
-
-    // let mut context = codegen::core::GrazeSb3GeneratorContext::new(context).unwrap();
-    // let visitor = codegen::core::GrazeSb3Generator;
-
-    // visitor.visit_graze_program(&parsed, &mut context).unwrap();
-
-    // for message in &context.messages {
-    //     dbg!(message);
-    // }
-
-    // dbg!(&context.asset_files);
-
-    // println!("{}", serde_json::to_string(&context.sb3).unwrap());
-
-    // zipper::write_to_zip_path(Path::new("./test.sb3"), &context).unwrap();
 }
