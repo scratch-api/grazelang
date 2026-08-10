5145160f20af531bfbf22c63553d2494dddc4dce
TheCommCraft <79996518+TheCommCraft@users.noreply.github.com>
2026-08-10 16:21:02 +0200

    resolve todo

--- grazelang/src/library.rs
+++ grazelang/src/library.rs
@@ -88,9 +88,6 @@ where
 
 generate_dynamic_generate_library!(pub);
 
-// TODO: Load extension as dynamic library using `dynamic_generate_library`
-// Issue: #85
-
 pub fn convert_generated_library(
     library: HashMap<String, LibraryItem>,
     symbol_table: &mut SymbolTable,
