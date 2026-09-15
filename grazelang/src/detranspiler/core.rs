b0da5cf2f8cf6fe3496d7b5fad4061c88c3f4a5a
TheCommCraft <79996518+TheCommCraft@users.noreply.github.com>
2026-09-15 10:59:52 +0200

    new todo

--- grazelang/src/detranspiler/core.rs
+++ grazelang/src/detranspiler/core.rs
@@ -821,6 +821,8 @@ pub fn convert_project(
 // TODO: Ensure that `grazelang` compiles with `#[cfg(not(feature = "detranspiler"))]`
 // Issue: #128
 
+// TODO: Implement formatted strings in detranspiler
+
 // A function is unbubbled iff it tries (`?`) any unbubbled result or returns a Err at any point without checking if
 // ExitOnError or ExitOnErrorUnlogged is on. A function is bubbled iff it is not unbubbled.
 // A Result is unbubbled iff it results from an unbubbled function or is an Err that is created without checking if
